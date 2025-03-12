4d9320c8a6dd92b70fff746fd6cda3f4e5863f1a
Andrade-Nil <95670676+Andrade-Nil@users.noreply.github.com>
2025-03-12 18:22:38 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-  return render_template('index.html')
-
+    return render_template('indice.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
-
+    app.run(debug=True)
