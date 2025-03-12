297442f5eaa41b801ad5e0d159730e367494e1cb
Andrade-Nil <95670676+Andrade-Nil@users.noreply.github.com>
2025-03-12 18:34:54 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
+
+# Servir arquivos estáticos manualmente se necessário
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
