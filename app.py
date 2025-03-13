036701e5a8f0f4b69c65b70305eb751797a2c0e1
Andrade-Nil <95670676+Andrade-Nil@users.noreply.github.com>
2025-03-12 23:21:55 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Caminho correto dos áudios
-path_music = "audio/"
-
-@app.route('/', methods=["GET"])
+@app.route('/')
 def index():
-    # Usando url_for para garantir que os arquivos sejam carregados corretamente
-    music_url = url_for('static', filename=path_music + "primeiro_audio_apresentacao.mp3")
-    return render_template('index.html', music=music_url)
+  return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
+
